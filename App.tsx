4842ed1856630666124ea203b87d785bb420ff18
Pedro Barros <pedrobarros0101@outlook.com>
2023-01-25 07:31:12 -0300

    [FEAT][GYM] - Telas de SignIn e SignUp

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
+import { Loading } from '@components/Loading';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { SigIn } from '@screens/SigIn';
 import { NativeBaseProvider } from 'native-base';
 import { Text, View, StatusBar } from 'react-native';
+import { THEME } from './src/theme';
+import { SigUp } from '@screens/SignUp';
 
 
 
@@ -8,14 +12,14 @@ import { Text, View, StatusBar } from 'react-native';
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular,  Roboto_700Bold});
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
         <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
         
         />
-        {fontsLoaded ? <View />: <View />}
+        {fontsLoaded ? <SigUp />: <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,51 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import BackgroundImg from "@assets/background.png";
+import LogoSVG from "@assets/logo.svg";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+
+export function SigUp(){
+    return(
+        <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
+
+        
+        <VStack flex={1} bg="gray.700" px={10} pb={16} >
+            <Image 
+            source={BackgroundImg}
+            alt="Pessoas treinando"
+            resizeMode="contain"
+            position="absolute"
+            />
+            <Center my={24}>
+                <LogoSVG />
+
+                <Text color="gray.100" fontSize={"sm"}>
+                    Treine sua mente e o seu corpo
+                </Text>
+            </Center>
+
+            <Center>
+                <Heading color="gray.100" fontSize={"xl"} mb={6} fontFamily={"heading"}
+                >
+                    Crie sua conta
+                </Heading>
+                <Input 
+                placeholder="Nome"
+                />
+                <Input 
+                placeholder="E-mail" 
+                keyboardType="email-address"
+                autoCapitalize="none"
+                />
+                <Input 
+                placeholder="Senha" 
+                secureTextEntry
+                />
+                <Button title="Criar e Acessar" />
+            </Center>
+            <Button title="Voltar para o login" variant="outline" mt={24} />
+        </VStack>
+        </ScrollView>
+    );
+}
